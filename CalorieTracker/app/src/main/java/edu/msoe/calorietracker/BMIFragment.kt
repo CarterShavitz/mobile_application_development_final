07d71f30806a23ff4a9035e0716ca11e1ada0e9d
cherukurus <cherukurus@msoe.edu>
2023-12-10 20:44:49 -0600

    Calorie Recommendations are done. However the actual recommendations are slightly higher in calories. Around 80 calories ish.

--- CalorieTracker/app/src/main/java/edu/msoe/calorietracker/BMIFragment.kt
+++ CalorieTracker/app/src/main/java/edu/msoe/calorietracker/BMIFragment.kt
@@ -7,8 +7,11 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import android.widget.RadioButton
+import android.widget.Spinner
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import kotlin.math.pow
 
@@ -27,6 +30,19 @@ class BMIFragment : Fragment() {
 
     private var listener: OnFragmentInteractionListener? = null
     private lateinit var bmiResultTextView: TextView
+
+    //Calorie Recommendations
+    private lateinit var activitySpinner: Spinner // Declare activitySpinner
+    private var weight: Double = 0.0
+    private var heightFeet: Double = 0.0
+    private var heightInches: Double = 0.0
+    private lateinit var ageEditText: EditText
+    private lateinit var maleRadioButton: RadioButton
+
+    private val MAINTAIN_WEIGHT_PERCENTAGE = 100
+    private val MILD_WEIGHT_LOSS_PERCENTAGE = 90
+    private val WEIGHT_LOSS_PERCENTAGE = 80
+    private val EXTREME_WEIGHT_LOSS_PERCENTAGE = 61
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -47,8 +63,14 @@ class BMIFragment : Fragment() {
         // Find the TextView by its ID
         bmiResultTextView = view.findViewById(R.id.bmi_result_text)
 
+        // Initialize activitySpinner
+        activitySpinner = view.findViewById(R.id.activity_spinner) // Replace with the actual ID of your Spinner
+        ageEditText = view.findViewById(R.id.age_edit_text)
+        maleRadioButton = view.findViewById(R.id.radio_male)
+
         val goBackButton: Button = view.findViewById(R.id.go_back)
         val calculateBMIButton: Button = view.findViewById(R.id.go_to_calorie_tracking_page)
+        val calculateCalorieButton: Button = view.findViewById(R.id.calorie_recom)
 
         goBackButton.setOnClickListener {
             listener?.onGoBackHomeButtonClick()
@@ -56,6 +78,10 @@ class BMIFragment : Fragment() {
 
         calculateBMIButton.setOnClickListener {
             calculateBMI()
+        }
+
+        calculateCalorieButton.setOnClickListener {
+            showCalorieRecommendations()
         }
 
         // Clear BMI result initially
@@ -114,9 +140,9 @@ class BMIFragment : Fragment() {
             return
         }
 
-        // Calculate BMI using the formula: weight (lb) / [height (in)]^2 x 703
-        val heightInInches = (heightFeet * 12) + heightInches
-        val bmi = (weight / (heightInInches.pow(2))) * 703
+        // Calculate BMI using the formula: weight (kg) / [height (m)]^2
+        val heightInMeters = ((heightFeet * 12) + heightInches) * 0.0254
+        val bmi = weight / (heightInMeters.pow(2))
 
         // Check if BMI is negative
         if (bmi < 0) {
@@ -149,5 +175,102 @@ class BMIFragment : Fragment() {
             bmi >= 40.0 -> "Severely Obese"
             else -> "Unknown"
         }
+    }
+
+    private fun showCalorieRecommendations() {
+        val weightEditText = view?.findViewById<EditText>(R.id.weight_edit_text)
+        val heightFeetEditText = view?.findViewById<EditText>(R.id.height_feet_edit_text)
+        val heightInchesEditText = view?.findViewById<EditText>(R.id.height_inches_edit_text)
+        val ageEditText = view?.findViewById<EditText>(R.id.age_edit_text)
+
+        val weightStr = weightEditText?.text.toString()
+        val heightFeetStr = heightFeetEditText?.text.toString()
+        val heightInchesStr = heightInchesEditText?.text.toString()
+        val ageStr = ageEditText?.text.toString()
+
+        // Check if any of the input fields is empty
+        if (weightStr.isEmpty() || heightFeetStr.isEmpty() || heightInchesStr.isEmpty() || ageStr.isEmpty()) {
+            showToast("Please enter all the required information.")
+            return
+        }
+
+        // Convert input values to appropriate types
+        val weightInput = weightStr.toDoubleOrNull() ?: run {
+            showToast("Please enter a valid weight.")
+            return
+        }
+
+        val heightFeetInput = heightFeetStr.toDoubleOrNull() ?: run {
+            showToast("Please enter a valid height (feet).")
+            return
+        }
+
+        val heightInchesInput = heightInchesStr.toDoubleOrNull() ?: run {
+            showToast("Please enter a valid height (inches).")
+            return
+        }
+
+        val ageInput = ageStr.toDoubleOrNull() ?: run {
+            showToast("Please enter a valid age.")
+            return
+        }
+
+        // Check for negative values
+        if (weightInput < 0 || heightFeetInput < 0 || heightInchesInput < 0 || ageInput < 0) {
+            showToast("Please enter non-negative values for weight, height, and age.")
+            return
+        }
+
+        // Calculate BMR using Mifflin-St Jeor Equation
+        val bmr = calculateBMRMifflinStJeor(weightInput, heightFeetInput, heightInchesInput, ageInput, isMale())
+
+        // Calculate daily calorie estimates based on specified rates
+        val maintainWeightCalories = calculateCalories(bmr, MAINTAIN_WEIGHT_PERCENTAGE)
+        val mildWeightLossCalories = calculateCalories(bmr, MILD_WEIGHT_LOSS_PERCENTAGE)
+        val weightLossCalories = calculateCalories(bmr, WEIGHT_LOSS_PERCENTAGE)
+        val extremeWeightLossCalories = calculateCalories(bmr, EXTREME_WEIGHT_LOSS_PERCENTAGE)
+
+        // Display the results
+        val dialogBuilder = AlertDialog.Builder(requireContext())
+        dialogBuilder.setTitle("Calorie Recommendations")
+
+        val message = """
+            Maintain weight: $maintainWeightCalories Calories/day
+            Mild weight loss (0.5 lb/week): $mildWeightLossCalories Calories/day
+            Weight loss (1 lb/week): $weightLossCalories Calories/day
+            Extreme weight loss (2 lb/week): $extremeWeightLossCalories Calories/day
+        """.trimIndent()
+
+        dialogBuilder.setMessage(message)
+        dialogBuilder.setPositiveButton("OK") { dialog, which ->
+            // Respond to the button click if needed
+        }
+
+        val dialog = dialogBuilder.create()
+        dialog.show()
+    }
+
+    private fun calculateCalories(bmr: Double, percentage: Int): Int {
+        val calories = ((bmr * percentage) / 100).toInt()
+        return if (calories >= 0) calories else 0
+    }
+
+    private fun calculateBMRMifflinStJeor(weight: Double, heightFeet: Double, heightInches: Double, age: Double, isMale: Boolean): Double {
+        // Convert height to centimeters
+        val heightInCm = ((heightFeet * 12) + heightInches) * 2.54
+
+        // Use the Mifflin-St Jeor Equation
+        return if (isMale) {
+            // BMR for men: 10W + 6.25H - 5A + 5
+            (10 * weight) + (6.25 * heightInCm) - (5 * age) + 5
+        } else {
+            // BMR for women: 10W + 6.25H - 5A - 161
+            (10 * weight) + (6.25 * heightInCm) - (5 * age) - 161
+        }
+    }
+
+    private fun isMale(): Boolean {
+        // Assume that the user has correctly selected their gender
+        return maleRadioButton.isChecked
     }
 }
